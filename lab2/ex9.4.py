34c322af264fe0714336bdd1af0336b2af543b74
vsfoff <smekhovaxd@gmail.com>
2021-10-28 19:31:33 +0300

    Update ex9.4.py

--- lab2/ex9.4.py
+++ lab2/ex9.4.py
@@ -1,21 +1,10 @@
-kolvo_holod = int(input())
-holodilnik = set()
-prod = set()
-
-for i in range(kolvo_holod):
-    holodilnik.add(input())
-recept = int(input())
-
-for i in range(recept):
-    name_recept = str(input())
-    prod_recept = int(input())
-    check = True
-    for q in range(prod_recept):
-        prod.add(input())
-    for w in prod:
-        if not w in kolvo_holod:
-            check = False
-
-    if check:
-        print(name_recept)
-    prod = set()
+main = set()
+for i in range(int(input())):
+    main.add(input())
+for i in range(int(input())):
+    recepy = input()
+    recep = set()
+    for j in range(int(input())):
+        recep.add(input())
+    if len(recep & main) == len(recep):
+        print(recepy)
