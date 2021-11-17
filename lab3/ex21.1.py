101ed81367629d2d77ff7480e2533f26551d4429
vsfoff <smekhovaxd@gmail.com>
2021-11-17 23:05:09 +0300

    Initial commit

--- lab3/ex21.1.py
+++ lab3/ex21.1.py
@@ -2,7 +2,8 @@ def from_string_to_list(string, container):
     container.extend(map(int, string.split()))
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+
     # a = [(input())]
     # from_string_to_list((input()), a)
     # print(*a)
